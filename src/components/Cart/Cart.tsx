74217938cb1dc914583fb2cb9efea2d8357e488e
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-07-22 19:22:52 +0200

    update quantity picker

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,3 +1,4 @@
+import QuantityPicker from "components/QuantityPicker/QuantityPicker";
 import { useCartContext } from "contexts/Cart.context";
 import { IProduct } from "mocks/products";
 import { useState } from "react";
@@ -8,16 +9,17 @@ const Cart = () => {
     const { products, resetCart, removeProduct, removeOne, addOne } = useCartContext();
     const reset = () => resetCart();
 
-
     return (
         <main>
             <ul>
                 {products.map((p) =>
                     <li key={p.id}>Article : {p.product.title} - Quantité :  {p.quantity}
                         <br />
-                        <button onClick={() => removeOne(p.product)}>-</button>
-                        <p>{p.quantity}</p>
-                        <button onClick={() => addOne(p.product, p.quantity)}>+</button>
+                        {/* <button onClick={() => removeOne(p.product)}>-</button> */}
+                         {/* <p>{p.quantity}</p> */}
+                    <QuantityPicker quantity={p.quantity} add={() => addOne(p.product, p.quantity)} remove={() => removeOne(p.product)} />
+
+                        {/* <button onClick={() => addOne(p.product, p.quantity)}>+</button> */}
                         <br /><br />
                         <button onClick={() => removeProduct(p.product)}>X</button>
                     </li>
